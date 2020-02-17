1765e536f9c9f33f1db71d3d42e5d815cf37624d
Jorrit Mesman <jpmesman@gmail.com>
2020-02-17 18:35:11 +0100

    Added run_lint.R function

---
+++ R/run_lint.R
@@ -0,0 +1,27 @@
+#' Runs lint for LER script
+#'
+#'Runs lintr::lint function, using the settings agreed upon for LakeEnsemblR
+#' @param file filepath; to R script that you want to run the lintr::lint function on
+#' @examples
+#'run_lint(file = "export_config.R")
+#'
+#' @importFrom lintr with_defaults lint
+
+run_lint <- function (file)
+{
+  LER_linters <- lintr::with_defaults(line_length_linter = line_length_linter(100), 
+                               commented_code_linter = NULL, 
+                               cyclocomp_linter = NULL, 
+                               paren_brace_linter = NULL, 
+                               spaces_left_parentheses_linter = NULL)
+  
+  file_location <- system.file(paste0("R/",file), package = packageName())
+  
+  # Call to the function, shows lines in "Markers" tab
+  lintr::lint("./R/export_meteo.R", linters = LER_linters)
+}
+
+
+
+
+
