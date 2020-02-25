4963a41a12ee7bf11295ac77f5a81b77cf2a470a
Jorrit Mesman <jpmesman@gmail.com>
2020-02-25 16:59:43 +0100

    Lintr get_wtemp_df.R input_json.R load_dic.R read_flake_out.R

--- R/get_wtemp_df.R
+++ R/get_wtemp_df.R
@@ -14,26 +14,26 @@
 #'
 #' @examples
 #' \dontrun{
-#' df <- get_wtemp_df(output = 'FLake/output/output.dat', folder = 'FLake', nml_file = 'FLake/feeagh.nml')
+#' df <- get_wtemp_df(output = "FLake/output/output.dat", folder = "FLake", nml_file = "FLake/feeagh.nml")
 #' }
 #'
 #' @export
-get_wtemp_df <- function(output, depths, folder = '.', nml_file, long = FALSE){
+get_wtemp_df <- function(output, depths, folder = ".", nml_file, long = FALSE){
 
-  met_file <- suppressWarnings(get_nml_value(arg_name = 'meteofile', nml_file = nml_file))
+  met_file <- suppressWarnings(get_nml_value(arg_name = "meteofile", nml_file = nml_file))
   met_file <- file.path(folder, met_file)
-  met_file <- gsub(',', '', met_file)
+  met_file <- gsub("," , "", met_file)
 
   met <- read.delim(met_file, header = FALSE)
-  datetime <- as.POSIXct(met[,ncol(met)], tz = 'UTC')
+  datetime <- as.POSIXct(met[, ncol(met)], tz = "UTC")
 
   # Code from R. I. Woolway FLake Workshop
   flake_out <- read.table(output, header = TRUE, skip = 1)
   # calculate temperature profile
-  Ts <- flake_out[['Ts']] # mixed layer (top) temperature
-  Tb <- flake_out[['Tb']] # bottom temperature
-  h <- flake_out[['h_ML']] # mixed layer depth
-  C <- flake_out[['C_T']] # shape factor
+  Ts <- flake_out[["Ts"]] # mixed layer (top) temperature
+  Tb <- flake_out[["Tb"]] # bottom temperature
+  h <- flake_out[["h_ML"]] # mixed layer depth
+  C <- flake_out[["C_T"]] # shape factor
   D <- max(depths) # mean depth
 
   if(long){
@@ -46,39 +46,46 @@ get_wtemp_df <- function(output, depths, folder = '.', nml_file, long = FALSE){
   depths <- depths[order(depths)]
 
 
-  for(kk in 1:length(depths)){
+  for(kk in seq_len(length(depths))){
     z <-  depths[kk]
     zeta <- (z - h) / (D - h)
     c1 <- 40 / 3
     c2 <- 20 / 3
     c3 <- 5 / 3
     c4 <- 10 / 3
-    is.in.ML <- z <= h
-    Tz <- ifelse(is.in.ML,Ts,zeta*(c1*C-c2+zeta*(18-30*C+zeta*(20*C-12+zeta*(c3-c4*C))))*(Tb-Ts)+Ts)
+    is_in_ML <- z <= h
+    if(is_in_ML){
+      Tz <- Ts
+    }else{
+      Tz <- zeta * (c1 * C - c2 + zeta *
+                      (18 - 30 * C + zeta * (20 * C - 12 + zeta * (c3 - c4 * C)))) *
+        (Tb - Ts) + Ts
+    }
+    
     if(long){
       dd <- data.frame(dateTime = flake_out$time,
                        depth = z,
                        wtr = Tz)
       l_wtr[[kk]] <- dd
     }else{
-      mat[,kk] <- Tz
+      mat[, kk] <- Tz
     }
   }
   if(long){
-    wtr2 <- do.call("rbind",l_wtr)
+    wtr2 <- do.call("rbind", l_wtr)
 
     # sort in temporal order
     idx <- sort(wtr2$dateTime, index.return = TRUE)$ix
-    wtr2 <- wtr2[idx,]
+    wtr2 <- wtr2[idx, ]
     colnames(wtr2) <- c("datetime", "Depth_meter", "Water_Temperature_celsius")
-    tims = sum((wtr2[,1] == wtr2[1,1]))
+    tims <- sum((wtr2[, 1] == wtr2[1, 1]))
     time_long <- rep(datetime, each = tims)
     wtr2$datetime <- time_long
   }else{
     wtr2 <- as.data.frame(mat)
     wtr2$datetime <- datetime
-    wtr2 <- wtr2[,c(ncol(wtr2), 1:(ncol(wtr2)-1))]
-    colnames(wtr2) <- c('datetime',paste('wtr_',depths, sep=""))
+    wtr2 <- wtr2[, c(ncol(wtr2), 1:(ncol(wtr2) - 1))]
+    colnames(wtr2) <- c("datetime", paste("wtr_", depths, sep = ""))
   }
   return(wtr2)
 }
