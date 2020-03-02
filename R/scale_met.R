fadb970cffe45f38e7c5c55cd5329aeb7193110a
JFeldbauer <johannes.feldbauer@posteo.de>
2020-03-02 10:18:39 +0100

    lointerized scale_met.R

--- R/scale_met.R
+++ R/scale_met.R
@@ -4,40 +4,46 @@
 #'
 #' @name scale_met
 #' @param met dataframe; in the format for that particular model with the column headers
-#' @param pars dataframe; scaling factors for meteorological data with column names corresponding to variable dictionary
-#' @param model character; Model for which scaling parameters will be applied. Options include c('GOTM', 'GLM', 'Simstrat', 'FLake')
+#' @param pars dataframe; scaling factors for meteorological data with column names corresponding
+#'    to variable dictionary
+#' @param model character; Model for which scaling parameters will be applied. Options include
+#'    c('GOTM', 'GLM', 'Simstrat', 'FLake')
 #' @param out_file filepath; For scaled meteorlogical data
 #' @export
-scale_met <- function(met, pars, model, out_file = NULL){
+scale_met <- function(met, pars, model, out_file = NULL) {
 
-  if('FLake' %in% model){
+  ## list with long standard names
+  l_names <- as.list(met_var_dic$standard_name)
+  names(l_names) <- met_var_dic$short.name
+
+  if("FLake" %in% model) {
     par_nams <- names(pars)
 
-    if('wind_speed' %in% par_nams){
-      met$Ten_Meter_Elevation_Wind_Speed_meterPerSecond <- met$Ten_Meter_Elevation_Wind_Speed_meterPerSecond * pars$wind_speed
+    if("wind_speed" %in% par_nams) {
+      met[[l_names$wind_speed]] <- met[[l_names$wind_speed]] * pars$wind_speed
     }
 
-    if('solar_radiation' %in% par_nams){
-      met$Shortwave_Radiation_Downwelling_wattPerMeterSquared <- met$Shortwave_Radiation_Downwelling_wattPerMeterSquared * pars$solar_radiation
+    if("solar_radiation" %in% par_nams) {
+      met[[l_names$swr]] <- met[[l_names$swr]] * pars$solar_radiation
     }
 
     # Write to file
-    write.table(met, out_file, sep = '\t', quote = FALSE, col.names = FALSE, row.names = FALSE)
+    write.table(met, out_file, sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
   }
 
-  if('GLM' %in% model){
+  if("GLM" %in% model) {
 
     par_nams <- names(pars)
 
-    if('wind_speed' %in% par_nams){
+    if("wind_speed" %in% par_nams) {
       met$WindSpeed <- met$WindSpeed * pars$wind_speed
     }
 
-    if('solar_radiation' %in% par_nams){
+    if("solar_radiation" %in% par_nams) {
       met$ShortWave <- met$ShortWave * pars$solar_radiation
     }
 
-    if('longwave_radiation' %in% par_nams){
+    if("longwave_radiation" %in% par_nams) {
       met$LongWave <- met$LongWave * pars$longwave_radiation
     }
 
@@ -45,43 +51,57 @@ scale_met <- function(met, pars, model, out_file = NULL){
     write.csv(met, out_file, quote = FALSE, row.names = FALSE)
   }
 
-  if('GOTM' %in% model){
+  if("GOTM" %in% model) {
 
     par_nams <- names(pars)
 
-    if('wind_speed' %in% par_nams){
-      met$Ten_Meter_Uwind_vector_meterPerSecond <- met$Ten_Meter_Uwind_vector_meterPerSecond * pars$wind_speed
-      met$Ten_Meter_Uwind_vector_meterPerSecond <- met$Ten_Meter_Uwind_vector_meterPerSecond * pars$wind_speed
+    if("wind_speed" %in% par_nams) {
+      met[[l_names$u10]] <- met[[l_names$u10]] * pars$wind_speed
+      met[[l_names$v10]] <- met[[l_names$v10]] * pars$wind_speed
     }
 
-    if('solar_radiation' %in% par_nams){
-      met$Shortwave_Radiation_Downwelling_wattPerMeterSquared <- met$Shortwave_Radiation_Downwelling_wattPerMeterSquared * pars$solar_radiation
+    if("solar_radiation" %in% par_nams) {
+      met[[l_names$swr]] <- met[[l_names$swr]] * pars$solar_radiation
     }
 
     # Write to file
-    write.table(met, out_file, quote = FALSE, row.names = FALSE, sep = '\t', col.names = TRUE)
+    write.table(met, out_file, quote = FALSE, row.names = FALSE, sep = "\t", col.names = TRUE)
 
   }
 
-  if('Simstrat' %in% model){
+  if("Simstrat" %in% model) {
 
     par_nams <- names(pars)
 
-    if('wind_speed' %in% par_nams){
-      met$Ten_Meter_Uwind_vector_meterPerSecond <- met$Ten_Meter_Uwind_vector_meterPerSecond * pars$wind_speed
-      met$Ten_Meter_Uwind_vector_meterPerSecond <- met$Ten_Meter_Uwind_vector_meterPerSecond * pars$wind_speed
+    if("wind_speed" %in% par_nams) {
+      met[[l_names$u10]] <- met[[l_names$u10]] * pars$wind_speed
+      met[[l_names$v10]] <- met[[l_names$v10]] * pars$wind_speed
     }
 
-    if('solar_radiation' %in% par_nams){
-      met$Shortwave_Radiation_Downwelling_wattPerMeterSquared <- met$Shortwave_Radiation_Downwelling_wattPerMeterSquared * pars$solar_radiation
+    if("solar_radiation" %in% par_nams) {
+      met[[l_names$swr]] <- met[[l_names$swr]] * pars$solar_radiation
     }
 
-    if('longwave_radiation' %in% par_nams){
-      met$Longwave_Radiation_Downwelling_wattPerMeterSquared <- met$Longwave_Radiation_Downwelling_wattPerMeterSquared * pars$longwave_radiation
+    if("longwave_radiation" %in% par_nams) {
+      met[[l_names$lwr]] <- met[[l_names$jwr]] * pars$longwave_radiation
     }
 
     # Write to file
     write.table(met, file = out_file, sep = "\t", quote = FALSE, row.names = FALSE)
+
+  }
+
+
+  if("MyLake" %in% model) {
+
+    if("wind_speed" %in% par_nams) {
+      met[[l_names$wind_speed]] <- met[[l_names$wind_speed]] * pars$wind_speed
+    }
+
+    if("solar_radiation" %in% par_nams) {
+      met[[l_names$swr]] <- met[[l_names$swr]] * pars$solar_radiation
+    }
+
 
   }
 
