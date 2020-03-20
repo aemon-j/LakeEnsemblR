c7bd4f715ff4e246c5d27f3dd7c02328c7dbb6d9
JFeldbauer <johannes.feldbauer@posteo.de>
2020-03-20 16:59:56 +0100

    added test to see if run_ensemble() works for Travis

--- tests/testthat/test_00_pkg_data.R
+++ tests/testthat/test_00_pkg_data.R
@@ -3,6 +3,43 @@ test_that("test data can be created", {
   testthat::skip_on_cran()
   library(LakeEnsemblR)
   
+  
   testthat::expect_error(run_ensemble(model = c("GRE")), 
                          'Unknown model: "GRE" in input argument "model"')
+})
+
+test_that("run model ensemble", {
+  
+  library(LakeEnsemblR)
+  library(gotmtools)
+  template_folder <- system.file("extdata/feeagh", package= 'LakeEnsemblR')
+  dir.create('example') # Create example folder
+  file.copy(from = template_folder, to = 'example', recursive = TRUE)
+  setwd('example/feeagh') # Change working directory to example folder
+  
+  # Set config file
+  masterConfigFile <- 'Feeagh_master_config.yaml'
+  
+  # 1. Example - creates directories with all model setup
+  export_config(config_file = masterConfigFile, model = c('FLake', 'GLM', 'GOTM', 'Simstrat', 'MyLake'),
+                folder = '.')
+  
+  # 2. Create meteo driver files
+  export_meteo(masterConfigFile, model = c('FLake', 'GLM', 'GOTM', 'Simstrat', 'MyLake'),
+               meteo_file = 'LakeEnsemblR_meteo_standard.csv')
+  
+  # 3. Create initial conditions
+  start_date <- get_yaml_value(file = masterConfigFile, label =  "time", key = "start")
+  
+  export_init_cond(masterConfigFile, model = c('FLake', 'GLM', 'GOTM', 'Simstrat', 'MyLake'),
+                   wtemp_file = 'LakeEnsemblR_wtemp_profile_standard.csv',
+                   date = start_date, 
+                   month = 1, ndeps = 2, print = TRUE)
+  
+  # 4. Run ensemble lake models
+  wtemp_list <- run_ensemble(config_file = masterConfigFile,
+                             model = c('FLake', 'GLM',  'GOTM', 'Simstrat', 'MyLake'),
+                             return_list = TRUE, create_netcdf = TRUE)
+  
+  testthat::expect_true(file.exists("output/ensemble_output.nc"))
 })
