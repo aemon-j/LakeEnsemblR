b4e03dd49ff710aa9b2b6676f23a23a961c4454c
JFeldbauer <johannes.feldbauer@posteo.de>
2020-04-10 13:30:38 +0200

    removed travis test run again

--- tests/testthat/test_00_pkg_data.R
+++ tests/testthat/test_00_pkg_data.R
@@ -41,32 +41,3 @@ test_that("create model meteo & config files", {
                           file.exists("GOTM/gotm.yaml") & file.exists("Simstrat/feeagh.par") &
                           file.exists("MyLake/mylake_config_template.Rdata")))
 })
-
-test_that("all models can be run", {
-
-  # Set config file
-  masterConfigFile <- 'Feeagh_master_config.yaml'
-  
-  # 1. Example - creates directories with all model setup
-  export_config(config_file = masterConfigFile, model = c('FLake', 'GLM', 'GOTM', 'Simstrat', 'MyLake'),
-                folder = '.')
-  
-  # 2. Create meteo driver files
-  export_meteo(masterConfigFile, model = c('FLake', 'GLM', 'GOTM', 'Simstrat', 'MyLake'),
-               meteo_file = 'LakeEnsemblR_meteo_standard.csv')
-  
-  # 3. Create initial conditions
-  start_date <- get_yaml_value(file = masterConfigFile, label =  "time", key = "start")
-  
-  export_init_cond(masterConfigFile, model = c('FLake', 'GLM', 'GOTM', 'Simstrat', 'MyLake'),
-                   wtemp_file = 'LakeEnsemblR_wtemp_profile_standard.csv',
-                   date = start_date, 
-                   month = 1, ndeps = 2, print = TRUE)
-
-# 4. Run ensemble lake models
-wtemp_list <- run_ensemble(config_file = masterConfigFile,
-                           model = c('FLake', 'GLM',  'GOTM', 'Simstrat', 'MyLake'),
-                           return_list = TRUE, create_netcdf = TRUE)
-
-testthat::expect_true(file.exists("output/ensemble_output.nc"))
-})
