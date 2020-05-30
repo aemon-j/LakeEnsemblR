0fc2af67a965d86695c356b53abcb39640294dcf
Jorrit Mesman <42146440+jorritmesman@users.noreply.github.com>
2020-05-30 16:29:41 +0200

    export_dirs.R sets up the directories for each model

---
+++ R/export_dirs.R
@@ -0,0 +1,129 @@
+#'Creates directories for each model
+#'
+#'Creates directories with file setups for each model, based on the master LakeEnsemblR config file
+#'
+#'@param config_file name of the master LakeEnsemblR config file
+#'@param model vector; model to export configuration file.
+#'  Options include c("GOTM", "GLM", "Simstrat", "FLake", "MyLake")
+#'@param folder folder
+#'@keywords methods
+#'@examples
+#'
+#'
+#'@export
+
+export_dirs <- function(config_file, model = c("GOTM", "GLM", "Simstrat", "FLake", "MyLake"),
+                          folder = "."){
+  # Set working directory
+  oldwd <- getwd()
+  setwd(folder)
+  
+  # this way if the function exits for any reason, success or failure, these are reset:
+  on.exit({
+    setwd(oldwd)
+  })
+  
+##---------------FLake-------------
+  if("FLake" %in% model){
+    # Create directory and output directory, if they do not yet exist
+    if(!dir.exists("FLake")){
+      dir.create("FLake")
+    }
+    if(!dir.exists("FLake/output")){
+      dir.create("FLake/output")
+    }
+    
+    # Read the FLake config file from config_file, and write it to the FLake directory
+    temp_fil <- get_yaml_value(config_file, "config_files", "FLake")
+    if(!file.exists(temp_fil)){
+      template_file <- system.file("extdata/flake_template.nml", package = packageName())
+      file.copy(from = template_file,
+                to = file.path(folder, temp_fil))
+    }
+  }
+  
+##---------------GLM-------------
+  if("GLM" %in% model){
+    # Create directory and output directory, if they do not yet exist
+    if(!dir.exists("GLM/output")){
+      dir.create("GLM/output", recursive = TRUE)
+    }
+    
+    # Read the GLM config file from config_file, and write it to the GLM directory
+    temp_fil <- get_yaml_value(config_file, "config_files", "GLM")
+    
+    if(!file.exists(temp_fil)){
+      template_file <- system.file("extdata/glm3_template.nml", package = packageName())
+      file.copy(from = template_file,
+                to = file.path(folder, temp_fil))
+    }
+  }
+  
+##---------------GOTM-------------
+  if("GOTM" %in% model){
+    # Create directory and output directory, if they do not yet exist
+    if(!dir.exists("GOTM/output")){
+      dir.create("GOTM/output", recursive = TRUE)
+    }
+    
+    # Read the GOTM config file from config_file, and write it to the GOTM directory
+    temp_fil <- get_yaml_value(config_file, "config_files", "GOTM")
+    if(!file.exists(temp_fil)){
+      template_file <- system.file("extdata/gotm_template.yaml", package = packageName())
+      file.copy(from = template_file,
+                to = file.path(folder, temp_fil))
+    }
+    
+    # Get output.yaml from the GOTMr package and copy to the GOTM folder
+    out_fil <- system.file("extdata/output.yaml", package = "GOTMr")
+    file.copy(from = out_fil, to = "GOTM/output.yaml")
+  }
+  
+##---------------Simstrat-------------
+  if("Simstrat" %in% model){
+    # Create directory and output directory, if they do not yet exist
+    if(!dir.exists("Simstrat/output")){
+      dir.create("Simstrat/output", recursive = TRUE)
+    }
+    
+    # Read the Simstrat config file from config_file, and write it to the Simstrat directory
+    temp_fil <- get_yaml_value(config_file, "config_files", "Simstrat")
+    if(!file.exists(temp_fil)){
+      template_file <- system.file("extdata/simstrat_template.par", package = packageName())
+      file.copy(from = template_file,
+                to = file.path(folder, temp_fil))
+    }
+    
+    # Copy in template files from examples folder in the package
+    qin_fil <- system.file("extdata/Qin.dat", package = "SimstratR")
+    qout_fil <- system.file("extdata/Qout.dat", package = "SimstratR")
+    tin_fil <- system.file("extdata/Tin.dat", package = "SimstratR")
+    sin_fil <- system.file("extdata/Sin.dat", package = "SimstratR")
+    file.copy(from = qin_fil, to = file.path(folder, "Simstrat", "Qin.dat"))
+    file.copy(from = qout_fil, to = file.path(folder, "Simstrat", "Qout.dat"))
+    file.copy(from = tin_fil, to = file.path(folder, "Simstrat", "Tin.dat"))
+    file.copy(from = sin_fil, to = file.path(folder, "Simstrat", "Sin.dat"))
+  }
+  
+##---------------MyLake-------------
+  if("MyLake" %in% model){
+    # Create directory and output directory, if they do not yet exist
+    if(!dir.exists("MyLake")){
+      dir.create("MyLake")
+    }
+    
+    # Load config file MyLake
+    temp_fil <- get_yaml_value(config_file, "config_files", "MyLake")
+    if(!file.exists(temp_fil)){
+      # Load template config file from extdata
+      mylake_path <- system.file(package = "LakeEnsemblR")
+      load(file.path(mylake_path, "extdata", "mylake_config_template.Rdata"))
+      
+      temp_fil <- gsub(".*/", "", temp_fil)
+      # save lake-specific config file for MyLake
+      save(mylake_config, file = file.path(folder, "MyLake", temp_fil))
+    }
+  }
+  
+  message("export_dirs complete!")
+}
