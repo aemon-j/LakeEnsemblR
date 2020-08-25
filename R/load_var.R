b06df7b2784d44027ec0a6c9eed0d03559f2c94c
JFeldbauer <johannes.feldbauer@posteo.de>
2020-08-25 13:47:39 +0200

    added test for allowed variable names to load_var

--- R/load_var.R
+++ R/load_var.R
@@ -5,7 +5,7 @@
 #' @param ncdf filepath; Name of the netCDF file to extract variable
 #' @param var character; Name of the variable to be extracted. Must match short name in netCDF file
 #' @param return character; Must be either list or array
-#' @param dim character; NetCDF dimensions to extract. Must be either "member" or "model". Defaults to "model". 
+#' @param dim character; NetCDF dimensions to extract. Must be either "member" or "model". Defaults to "model".
 #' @param dim_index numeric; Index of dimension chosen to extract from. Defaults to 1.
 #' @param print logical; Print the name and units of the variable extracted, defaults to TRUE
 #' @return dataframe in the same format as the observation file with the surface in the top column and the bottom in the last column.
@@ -16,17 +16,23 @@
 
 #' @export
 load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, print = TRUE){
-  
+
   match.arg(return, c("list", "array"))
   match.arg(dim, c("model", "member"))
-  
+
   if(!file.exists(ncdf)) {
     stop(ncdf, " does not exist. Check the filepath is correct.")
   }
-  
+
+  # check if variable is an allowed name
+  if(!var %in% lake_var_dic$short_name) {
+    stop(paste0("Variabel '", var, "' unknown. Allowed names for var: ",
+                paste0(lake_var_dic$short_name, collapse = ", ")))
+  }
+
   tryCatch({
     fid <- ncdf4::nc_open(ncdf) # Open netCDF
-    
+
     # Extract the time
     tim <- ncdf4::ncvar_get(fid, "time")
     tunits <- ncdf4::ncatt_get(fid, "time")
@@ -43,22 +49,22 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
                                 "-", tday, " ", thour, ":", tmin),
                          format = "%Y-%m-%d %H:%M", tz = "UTC")
     time <- as.POSIXct(tim, origin = origin, tz = "UTC")
-    
+
     # Extract model names
     mod_names <- ncdf4::ncatt_get(fid, "model", "Model")$value
     mod_names <- strsplit(mod_names, ", ")[[1]]
     mod_names <- substring(mod_names, 5)
-    
+
     # Extract members
     mem <- ncdf4::ncvar_get(fid, "member")
-    
+
     # Extract variable
     var1 <- ncdf4::ncvar_get(fid, var)
     tunits <- ncdf4::ncatt_get(fid, var)
-    miss_val <- tunits$missing_value 
+    miss_val <- tunits$missing_value
     var1[var1 >= miss_val] <- NA # Replace large values with NAs
     var_dim <- strsplit(tunits$coordinates, " ")[[1]]
-    
+
     # Extract depths if dimensions are greater than 2
     if(length(dim(var1)) > 2){
       z <- ncvar_get(fid, "z")
@@ -72,7 +78,7 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
   }, finally = {
     ncdf4::nc_close(fid) # Close netCDF file
   })
-  
+
   mat <- matrix(data = c(var, tunits$units, tunits$coordinates),
                dimnames = list(c("short_name",
                                  "units", "dimensions"), c()))
@@ -80,7 +86,7 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
     message("Extracted ", var, " from ", ncdf)
     print(mat)
   }
-  
+
   if(length(dim(var1)) == 4) {
     n_vals <- dim(var1)[2] * (dim(var1)[3]) * (dim(var1)[4])
     # Check for empty members
@@ -106,11 +112,11 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
       var1 <- var1[(seq_len(m-1)), , ]
     }
   }
-  
-  
-  
+
+
+
   if(return == "array"){
-    
+
     if(length(dim(var1)) == 4) {
       dimnames(var1) <- list(paste0("member_", seq_len(dim(var1)[1])),
                              mod_names, as.character(time), z)
@@ -125,34 +131,34 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
     if(length(dim(var1)) == 2) {
       dimnames(var1) <- list(mod_names, as.character(time))
     }
-    
+
     return(var1)
   }
-  
+
   if(return == "list"){
     # 3-D var w/member ----
     if("z" %in% var_dim) {
       if(length(dim(var1)) == 4){
         if(dim == "model") {
-          
+
           if ( dim_index > dim(var1)[1] ) {
             stop("Dimension index ", dim_index, " out of bounds!\nAvailable dimensions: ",
                  paste(seq_len(dim(var1)[1]), collapse = ","))
           }
-          
+
           var_list <- lapply(seq(dim(var1)[2]), function(x)var1[dim_index, x, , ])
           names(var_list) <- mod_names
         } else if ( dim == "member" ) {
-          
+
           if (dim_index > dim(var1)[2]) {
             stop("Dimension index ", dim_index, " out of bounds!\nAvailable dimensions: ",
                  paste(seq_len(dim(var1)[2]), collapse = ","))
           }
-          
+
           var_list <- lapply(seq(dim(var1)[1]), function(x)var1[x, dim_index, , ])
-          
+
           n_vals <- dim(var_list[[1]])[1] * (dim(var_list[[1]])[2])
-          
+
           # Check for empty members
           for(m in seq_len(length(var_list))) {
             nas <- sum(is.na(var_list[[m]]))
@@ -165,9 +171,9 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
           }
           names(var_list) <- paste0(mod_names[dim_index], "_member_",
                                     seq_len(length(var_list)))
-          
+
         }
-        
+
         # Add datetime column + columns names for rLake Analyzer
         var_list <- lapply(var_list, function(x){
           x <- as.data.frame(x)
@@ -179,7 +185,7 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
       } else if(length(dim(var1)) == 3) {
         var_list <- lapply(seq(dim(var1)[1]), function(x)var1[x, , ])
         names(var_list) <- mod_names
-        
+
         # Add datetime column + columns names for rLake Analyzer
         var_list <- lapply(var_list, function(x){
           x <- as.data.frame(x)
@@ -188,13 +194,13 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
           return(x)
         })
       }
-    
+
   # 2-D variables no members ----
     } else {
       if (length(dim(var1)) == 2) {
         var_list <- lapply(seq(dim(var1)[1]), function(x)var1[x, ])
         names(var_list) <- mod_names
-        
+
         # Add datetime column + columns names for rLake Analyzer
         var_list <- lapply(var_list, function(x){
           x <- as.data.frame(x)
@@ -204,18 +210,18 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
         })
         # 2-D w/ members ----
       } else if (length(dim(var1)) == 3) {
-        
+
         if(dim == "model") {
-          
+
           if ( dim_index > dim(var1)[2] ) {
             stop("Dimension index ", dim_index, " out of bounds!\nAvailable dimensions: ",
                  paste(seq_len(dim(var1)[2]), collapse = ","))
           }
-          
+
           var_list <- lapply(seq(dim(var1)[2]), function(x)var1[dim_index, x, ])
           names(var_list) <- mod_names
         } else {
-          
+
           var_list <- lapply(seq(dim(var1)[1]), function(x)var1[x, dim_index, ])
           n_vals <- length(var_list[[1]])
           # Check for empty members
@@ -231,7 +237,7 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
           names(var_list) <- paste0(mod_names[dim_index], "_member_",
                                     seq_len(length(var_list)))
         }
-        
+
         # Add datetime column + columns names for variable
         var_list <- lapply(var_list, function(x){
           x <- as.data.frame(x)
@@ -239,7 +245,7 @@ load_var <- function(ncdf, var, return = "list", dim = "model", dim_index = 1, p
           colnames(x)[2] <- var
           return(x)
         })
-        
+
       }
     }
   }
