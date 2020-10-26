b1d0288da5865605b666df809a3fb7c9a1aaa2ae
JFeldbauer <johannes.feldbauer@posteo.de>
2020-10-26 12:14:26 +0100

    updated description, namespace and manuals

--- R/export_config.R
+++ R/export_config.R
@@ -8,9 +8,9 @@
 #'@param dirs boolean; create directories for each model and if needed copies templates.
 #'  Calls export_dirs. Defaults to TRUE
 #'@param time boolean; exports time settings. Calls export_time. Defaults to TRUE.
-#'@param location boolean; exports location and hypsograph settings. 
-#'  Calls export_location. Defaults to TRUE. 
-#'@param output_settings boolean; exports output settings. 
+#'@param location boolean; exports location and hypsograph settings.
+#'  Calls export_location. Defaults to TRUE.
+#'@param output_settings boolean; exports output settings.
 #'  Calls export_output_settings. Defaults to TRUE.
 #'@param meteo boolean; export meteorology data.
 #'  Calls export_meteo. Defaults to TRUE.
@@ -39,69 +39,69 @@ export_config <- function(config_file, model = c("GOTM", "GLM", "Simstrat", "FLa
   # Set working directory
   oldwd <- getwd()
   setwd(folder)
-  
+
   # Fix time zone
   original_tz <- Sys.getenv("TZ")
-  
+
   # this way if the function exits for any reason, success or failure, these are reset:
   on.exit({
     setwd(oldwd)
     Sys.setenv(TZ = original_tz)
   })
-  
+
   Sys.setenv(TZ = "GMT")
-  
+
   # Check if config file exists
   if(!file.exists(config_file)){
     stop(config_file, " does not exist.")
   }
-  
+
   # check the master config file
   check_master_config(config_file, exp_cnf = TRUE)
   # check model input
   model <- check_models(model)
-  
+
 ##--------------------- Export sub-functions ---------------
   # Export directories and copy template files
   if(dirs){
     export_dirs(config_file = config_file, model = model, folder = folder)
   }
-  
+
   # Export time settings (start, stop, time step)
   if(time){
     export_time(config_file = config_file, model = model, folder = folder)
   }
-  
+
   # Export hypsograph, lat/lon, ice
   if(location){
     export_location(config_file = config_file, model = model, folder = folder)
   }
-  
+
   # Export output_settings (depth and time intervals)
   if(output_settings){
     export_output_settings(config_file = config_file, model = model, folder = folder)
   }
-  
+
   # Export meteo
   if(meteo){
     export_meteo(config_file = config_file, model = model, folder = folder)
   }
-  
+
   # Export initial conditions
   if(init_cond){
     export_init_cond(config_file = config_file, model = model, print = TRUE, folder = folder)
-  } 
-  
+  }
+
   # Export light extinction (Kw)
   if(extinction){
     export_extinction(config_file = config_file, model = model, folder = folder)
   }
-  
+
   # Export user-defined inflow boundary condition
   if(inflow){
     export_inflow(config_file = config_file, model = model, folder = folder)
   }
-  
+
   # Export user-defined model-specific parameters
   if(model_parameters){
     export_model_parameters(config_file = config_file, model = model, folder = folder)
