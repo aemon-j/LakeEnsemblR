973e93a7f71f92acaaae47d77a5195c5af9cf901
Johannes Feldbauer <johannes.feldbauer@posteo.de>
2021-05-17 11:00:37 +0200

    added missing space in warning message

--- R/run_ensemble.R
+++ R/run_ensemble.R
@@ -93,7 +93,7 @@ run_ensemble <- function(config_file, model = c("GOTM", "GLM", "Simstrat", "FLak
     # check if all entries are unique
     if(any(duplicated(paste0(obs$datetime, obs$Depth_meter)))) {
       warning(paste0("There are non-unique observations in the observed",
-                     "water temperature file ", obs_file, "!"))
+                     " water temperature file ", obs_file, "!"))
     }
     
     # check for NAs in the observation
