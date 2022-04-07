fa5424c199adeba40b4c15139768af936d2c7926
Tadhg Moore <tadhgm@vt.edu>
2022-04-07 14:55:27 +0000

    whitespace

--- R/scale_met.R
+++ R/scale_met.R
@@ -11,121 +11,121 @@
 #' @param out_file filepath; For scaled meteorlogical data
 #' @export
 scale_met <- function(met, pars, model, out_file = NULL) {
-  
+
   # Load Rdata
   data("met_var_dic", package = "LakeEnsemblR", envir = environment())
-  
+
   ## list with long standard names
   l_names <- as.list(met_var_dic$standard_name)
   names(l_names) <- met_var_dic$short_name
-  
+
   if("FLake" %in% model) {
     par_nams <- names(pars)
-    
+
     if("wind_speed" %in% par_nams) {
       met[[l_names$wind_speed]] <- met[[l_names$wind_speed]] * pars$wind_speed
     }
-    
+
     if("swr" %in% par_nams) {
       met[[paste0("!", l_names$swr)]] <- met[[paste0("!", l_names$swr)]] * pars$swr
     }
-    
+
     #Reduce number of digits
     met[, -ncol(met)] <- signif(met[, -ncol(met)], digits = 8)
-    
+
     # Write to file
     write.table(met, out_file, sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
   }
-  
+
   if("GLM" %in% model) {
-    
+
     par_nams <- names(pars)
-    
+
     if("wind_speed" %in% par_nams) {
       met$WindSpeed <- met$WindSpeed * pars$wind_speed
     }
-    
+
     if("swr" %in% par_nams) {
       met$ShortWave <- met$ShortWave * pars$swr
     }
-    
+
     if("lwr" %in% par_nams) {
       met$LongWave <- met$LongWave * pars$lwr
     }
-    
+
     #Reduce number of digits
     met[, -1] <- signif(met[, -1], digits = 8)
-    
+
     # Write to file
     write.csv(met, out_file, quote = FALSE, row.names = FALSE)
   }
-  
+
   if("GOTM" %in% model) {
-    
+
     par_nams <- names(pars)
-    
+
     if("wind_speed" %in% par_nams) {
       met[[l_names$u10]] <- met[[l_names$u10]] * pars$wind_speed
       met[[l_names$v10]] <- met[[l_names$v10]] * pars$wind_speed
     }
-    
+
     if("swr" %in% par_nams) {
       met[[l_names$swr]] <- met[[l_names$swr]] * pars$swr
     }
-    
+
     #Reduce number of digits
     met[, -1] <- signif(met[, -1], digits = 8)
-    
+
     # Write to file
     write.table(met, out_file, quote = FALSE, row.names = FALSE, sep = "\t", col.names = TRUE)
-    
+
   }
-  
+
   if("Simstrat" %in% model) {
-    
+
     par_nams <- names(pars)
-    
+
     if("wind_speed" %in% par_nams) {
       met[[l_names$u10]] <- met[[l_names$u10]] * pars$wind_speed
       met[[l_names$v10]] <- met[[l_names$v10]] * pars$wind_speed
     }
-    
+
     if("swr" %in% par_nams) {
       met[[l_names$swr]] <- met[[l_names$swr]] * pars$swr
     }
-    
+
     if("lwr" %in% par_nams) {
       met[[l_names$lwr]] <- met[[l_names$lwr]] * pars$lwr
     }
-    
+
     #Reduce number of digits
     met[, -1] <- signif(met[, -1], digits = 8)
-    
+
     # Write to file
     write.table(met, file = out_file, sep = "\t", quote = FALSE, row.names = FALSE)
-    
+
   }
-  
-  
+
+
   if("MyLake" %in% model) {
-    
+
     par_nams <- names(pars)
-    
+
     if("wind_speed" %in% par_nams) {
       met[[l_names$wind_speed]] <- met[[l_names$wind_speed]] * pars$wind_speed
     }
-    
+
     if("swr" %in% par_nams) {
       met[[l_names$swr]] <- met[[l_names$swr]] * pars$swr
     }
-    
+
     #Reduce number of digits
     met[, -1] <- signif(met[, -1], digits = 8)
-    
+
     # Write to file
     write.table(met, file = out_file, sep = "\t", quote = FALSE, row.names = FALSE,
                 col.names = FALSE)
-    
+
   }
-  
+
 }
